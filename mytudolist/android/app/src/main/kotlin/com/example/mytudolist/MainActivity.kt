d4eb77433ddd66054c348d4f95587233adea1f25
ttec12 <80103060+ttec12@users.noreply.github.com>
2024-10-22 04:34:56 +0000

    Add files via upload

---
+++ mytudolist/android/app/src/main/kotlin/com/example/mytudolist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mytudolist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mytudolist/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme:ThemeData(
+      brightness: Brightness.light,
+      primaryColor: Colors.bule,
+      accentColor: Colors.orange
+    ),
+    home:
+  ));
+
+
+  
